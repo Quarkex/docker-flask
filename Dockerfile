8ed38460da542f2c93c8e95243baa8b7dff7d238
Manlio García <quarkex@gmail.com>
2018-09-13 08:39:54 +0100

    successfully exposed default app files

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ MAINTAINER Manlio García "manliojoaquin@gmail.com"
 RUN apt-get update -y
 RUN apt-get install -y python-pip python-dev build-essential
 COPY . /app
-WORKDIR /app
-RUN pip install -r requirements.txt
-ENTRYPOINT ["python"]
-CMD ["app.py"]
+COPY default_cmd.sh /
+WORKDIR /mnt
+ENTRYPOINT ["bash"]
+CMD ["/default_cmd.sh"]
---
+++ default_cmd.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [[ ! -e /mnt/app ]]; then
+    if [[ -e /app ]]; then
+        mv /app /mnt/app
+    fi
+fi
+
+if [[ -f /mnt/app.py ]]; then
+
+    if [[ -f /mnt/requirements.txt ]]; then
+        if [[ -f /var/cache/pip_cache ]]; then
+            # check if files are different
+            diff /var/cache/pip_cache /mnt/requirements.txt 2&>/dev/null
+            # if they are...
+            if [[ $? == 1 ]]; then
+                cp /mnt/requirements.txt /var/cache/pip_cache
+                pip install -r /mnt/requirements.txt
+            fi
+        else
+            cp /mnt/requirements.txt /var/cache/pip_cache
+            pip install -r /mnt/requirements.txt
+        fi
+    fi
+
+    /usr/bin/env python /mnt/app.py
+else
+    exec bash
+fi
